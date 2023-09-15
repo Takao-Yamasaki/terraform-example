285ee35ebb693d87843258eaaaadc3e8e9f6f083
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-16 08:58:42 +0900

    add:変数の定義

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,19 @@
+# デフォルト値の定義
+variable "example_instance_type" {
+	default = "t3.micro"
+}
+
 # EC2インスタンスの定義
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
 
   tags = {
     Name = "example"
   }
+  user_data = <<EOF
+	#!/bin/bash
+	yum install -y httpd
+	systemctl start httpd.service
+EOF
 }
