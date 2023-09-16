b563b14020593a47319c65df593ffcda81b4e595
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-16 09:19:55 +0900

    add:出力値の定義

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,11 @@ locals {
 	example_instance_type = "t3.micro"
 }
 
+# 出力値の定義
+output "example_instance_id" {
+	value = aws_instance.example.id
+}
+
 # EC2インスタンスの定義
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
