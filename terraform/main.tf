85ac8465e628e7246df47ff7cf4734e28c003147
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-16 09:07:53 +0900

    add:ローカル変数の定義

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
-# デフォルト値の定義
-variable "example_instance_type" {
-	default = "t3.micro"
+# ローカル変数の定義
+locals {
+	example_instance_type = "t3.micro"
 }
 
 # EC2インスタンスの定義
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
-  instance_type = var.example_instance_type
+  instance_type = local.example_instance_type
 
   tags = {
     Name = "example"
