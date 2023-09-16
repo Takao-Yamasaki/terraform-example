f4fb72339909a264acf878bc11516158b6f4cdf7
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-16 10:03:15 +0900

    add:データソースの定義

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,28 @@ output "example_instance_id" {
 	value = aws_instance.example.id
 }
 
+# データソースの定義
+# 最新のAmazon Linux2のAMIを参照
+data "aws_ami" "recent_amazon_linux_2" {
+	# 最新のAMIを取得
+	most_recent = true
+	owners = ["amazon"]
+
+	# 検索条件を指定
+	filter {
+		name = "name"
+		values = ["amzn2-ami-amd-hvm-2.0.20230906.0-x86_64-gp2"]
+	}
+
+	filter {
+		name = "state"
+		values = ["available"]
+	}
+}
+
 # EC2インスタンスの定義
 resource "aws_instance" "example" {
-  ami = "ami-0c3fd0f5d33134a76"
+  ami = data.aws_ami.recent_amazon_linux_2.image_id
   instance_type = local.example_instance_type
 
   tags = {
